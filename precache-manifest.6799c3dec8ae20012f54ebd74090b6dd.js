412f316fb328996326a1ad94857c2a2ab0ef4829
Isaiah David <isaiahd10@icloud.com>
2023-02-14 09:52:06 -0500

    Updates

---
+++ precache-manifest.6799c3dec8ae20012f54ebd74090b6dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3887b84beb92171a3435",
+    "url": "/Business-Card/static/css/main.e30e01a1.chunk.css"
+  },
+  {
+    "revision": "3887b84beb92171a3435",
+    "url": "/Business-Card/static/js/main.3887b84b.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/Business-Card/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "99ea9b05c4a37375ae93",
+    "url": "/Business-Card/static/js/2.99ea9b05.chunk.js"
+  },
+  {
+    "revision": "30eb099c6558346211b2",
+    "url": "/Business-Card/static/js/runtime~main.30eb099c.js"
+  },
+  {
+    "revision": "ae5b638917d7f7a778f4d41bc11df28e",
+    "url": "/Business-Card/static/media/1.ae5b6389.png"
+  },
+  {
+    "revision": "63dc0ca18e6648be5ed52952f165deef",
+    "url": "/Business-Card/index.html"
+  }
+];
