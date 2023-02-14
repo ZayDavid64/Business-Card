6a96001dc36bf9e052851a254e9505b23d3319c3
Isaiah David <isaiahd10@icloud.com>
2023-02-14 00:06:40 -0500

    Add packages

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="who">
       <h3>Who Are We?</h3>
       <p className="about">We Are a Black Owned Business focused on creating styles that are functional as they are timeless.
       Our hair is our crown, and our skin, like shining armour. We deserve clothes built to protect and enrich them.
       That is why we created the Silk Collective, not only to focus on clothing that safeguards
       our hair and skin, but also to preserve and augment the culture and those who build it.</p>
+      </div>
+      <footer>
+        
+        </footer>
     </div>
   );
 }
